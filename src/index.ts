abe381bcd92fe7a87a0266fb31db02caad1aad45
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2020-04-22 16:04:41 +0200

    Fixes and improvements

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,26 @@ import crypto from 'crypto'
 import * as core from '@actions/core'
 import { context } from '@actions/github'
 
-const setOutput = (key, value) => {
-  console.log(`Setting output: ${key}=${value}`)
-  core.setOutput(key, value)
-}
-
 async function main() {
   if (context.payload.action !== 'labeled') {
-    setOutput('hasLabel', 'false')
+    core.setOutput('hasLabel', 'false')
     return
   }
 
   const { label } = context.payload as Payload
 
   if (!label || typeof label.name !== 'string' || !label.name.startsWith('env:')) {
-    setOutput('hasLabel', 'false')
+    core.setOutput('hasLabel', 'false')
     return
   }
 
-  const name = crypto.randomBytes(8).toString('hex')
+  const prNumber = context.payload.pull_request!.number
+  const name = crypto.createHash('md5').update(String(prNumber)).digest('hex')
   const [, env] = label.name.split(':')
 
-  setOutput('hasLabel', 'true')
-  setOutput('name', name)
-  setOutput('env_vars', env.startsWith('dev') ? 'DEVELOPMENT=true' : 'PRODUCTION=true')
+  core.setOutput('hasLabel', 'true')
+  core.setOutput('name', name)
+  core.setOutput('env', env)
 }
 
 try {
